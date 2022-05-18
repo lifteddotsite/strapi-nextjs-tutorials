e1cec815bab6b771ff3f03215577cd4f3b4d4611
junteudjio <jun.teudjio@gmail.com>
2022-05-18 14:57:36 +0100

    t7 - done

--- pages/api/test-strapi.js
+++ pages/api/test-strapi.js
@@ -1,8 +1,8 @@
 import datasource from '../../datalayer';
 
 export default async function handler(req, res) {
-  const data = await datasource.getJobBySlug({
-    slug: 'junior-software-engineer-next-js-and-reactjs',
+  const data = await datasource.getJobsByCompanyId({
+    id: 2,
   });
   res.status(200).json({ data });
 }
