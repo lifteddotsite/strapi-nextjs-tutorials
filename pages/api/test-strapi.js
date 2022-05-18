6091d9503283323ecfda95e4a7154ce2b56c7f87
junteudjio <jun.teudjio@gmail.com>
2022-05-18 14:42:46 +0100

    t6 - done

--- pages/api/test-strapi.js
+++ pages/api/test-strapi.js
@@ -1,8 +1,8 @@
 import datasource from '../../datalayer';
 
 export default async function handler(req, res) {
-  const data = await datasource.getCompanyBySlug({
-    slug: 'lifted-ventures-ltd',
+  const data = await datasource.getJobBySlug({
+    slug: 'junior-software-engineer-next-js-and-reactjs',
   });
   res.status(200).json({ data });
 }
