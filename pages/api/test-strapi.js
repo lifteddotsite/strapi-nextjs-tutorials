43368930307d85e116e6f0812e53cdd1c32a6171
junteudjio <jun.teudjio@gmail.com>
2022-05-18 15:43:16 +0100

    t12 - done

--- pages/api/test-strapi.js
+++ pages/api/test-strapi.js
@@ -1,10 +1,11 @@
 import datasource from '../../datalayer';
 
 export default async function handler(req, res) {
-  // const data = await datasource.searchJobs({ remoteOkOnly: true });
   const data = await datasource.searchJobs({
-    minBaseSalary: 70000,
-    maxBaseSalary: 120000,
+    experienceLevels: ['Junior', 'Tech-lead'],
   });
+  // const data = await datasource.searchJobs({
+  //   jobTypes: ['Full-time', 'Internship'],
+  // });
   res.status(200).json({ data });
 }
